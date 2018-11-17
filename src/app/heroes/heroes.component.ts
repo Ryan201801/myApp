3ae399c518b689925461ce8cb328aacc6b95afcc
Ryan201801 <815267338@qq.com>
2018-11-18 00:57:04 +0800

    test

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,15 +8,22 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
+  
   hero: Hero = {
     id:1,
     name:"Winstorm"
-  };
+  }; 
 
   constructor() { }
 
   ngOnInit() {
 
   }
-
+  
+	selectedHero: Hero;
+	onSelect(hero: Hero): void {
+		
+	  this.selectedHero = hero;
+	  alert(this.selectedHero.name);
+	}
 }
