dbaba542641d87767c08a4a6ddef22e4f163b8f8
Ryan201801 <815267338@qq.com>
2018-11-19 23:27:50 +0800

    路由

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,14 +2,21 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
-
+import { Observable} from 'rxjs';
+import { of } from 'rxjs/observable/of';
 @Injectable()
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-	getHeroes(): Hero[] {
+	/*getHeroes(): Hero[] {
 		this.messageService.add('HeroService: fetched heroes');
 	  return HEROES;
-	}
+	}*/
+	
+	getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
 }
